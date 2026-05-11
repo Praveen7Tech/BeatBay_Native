22c23cd474162a87f427c088e1ab15a01d7a1a49
Praveen <praveen.tech.js@gmail.com>
2026-05-11 21:31:58 +0530

    created miniplayer and close gesture update

--- app/(tabs)/(home)/_layout.tsx
+++ app/(tabs)/(home)/_layout.tsx
@@ -1,40 +1,46 @@
+import MiniPlayer from "@/components/MiniPlayer";
 import { tabs } from "@/constants/data";
 import { Tabs } from "expo-router";
+import { View } from "react-native";
 
-export default function TabLayout(){
-    return(
-        <Tabs screenOptions={{
-            headerShown:false,
-            tabBarActiveTintColor: "#FFFFFF",
-            tabBarInactiveTintColor:"#B3B3B3",
-            tabBarStyle:{
-                backgroundColor: "rgba(0, 0, 0, 0.85)",
-                position: "absolute", //allow content to scroll behind bar
-                borderTopWidth:0,
-                elevation:0,
-            },
-            tabBarLabelStyle:{
-                fontFamily: "sans-semibold"
-            }
-        }}>
-           {tabs.map((tab)=>(
-            <Tabs.Screen 
-                key={tab.name}
-                name={tab.name}
-                options={{
-                    title: tab.title,
-                    tabBarIcon: ({color, focused})=> (
-                        <tab.icon 
-                            size={26} 
-                            strokeWidth={focused ? 2.5 : 2} 
-                            color={color}
-                            //fill={focused ? color : "none"}
-                        />
-                    )
-                }}
-            />
-           ))}
-            
-        </Tabs>
+export default function TabLayout() {
+    return (
+        <View style={{flex:1}}>
+            <Tabs screenOptions={{
+                headerShown: false,
+                tabBarActiveTintColor: "#FFFFFF",
+                tabBarInactiveTintColor: "#B3B3B3",
+                tabBarStyle: {
+                    backgroundColor: "rgba(0, 0, 0, 0.85)",
+                    position: "absolute", //allow content to scroll behind bar
+                    borderTopWidth: 0,
+                    elevation: 0,
+                },
+                tabBarLabelStyle: {
+                    fontFamily: "sans-semibold"
+                }
+            }}>
+                {tabs.map((tab) => (
+                    <Tabs.Screen
+                        key={tab.name}
+                        name={tab.name}
+                        options={{
+                            title: tab.title,
+                            tabBarIcon: ({ color, focused }) => (
+                                <tab.icon
+                                    size={26}
+                                    strokeWidth={focused ? 2.5 : 2}
+                                    color={color}
+                                //fill={focused ? color : "none"}
+                                />
+                            )
+                        }}
+                    />
+                ))}
+
+            </Tabs>
+
+            <MiniPlayer/>
+        </View>
     )
 }
